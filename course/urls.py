d1071ea99dfd51d7f05f9223be1a7b47761babe7
athulya <athulyaathu.uk@gmail.com>
2023-09-30 20:04:58 +0530

    completed chapter adding

--- course/urls.py
+++ course/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('category/',views.CategoryList.as_view()),
     path('course/',views.CourseList.as_view()),
     path('chapter/',views.ChapterList.as_view()),
+    path('coursechapters/<int:course_id>',views.CourseChapterList.as_view()),
 ]
